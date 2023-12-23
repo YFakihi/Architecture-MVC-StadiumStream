608924b8a7ce5aeaa0b83c401afb64076a974e54
yfakihi <fakihiyassin01@gmail.com>
2023-12-23 12:23:15 +0100

    ADDED CRUD PHP MVC

---
+++ app/models/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+class DatabaseConnection {
+    private $conn;
+
+    public function __construct($dbHost, $dbName, $dbUsername, $dbPassword) {
+            $this->conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
+?>
