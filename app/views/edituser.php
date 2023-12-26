685a8a08ea4da32d5831294e1c13744a84aae775
yfakihi <fakihiyassin01@gmail.com>
2023-12-26 15:56:58 +0100

    PHP MVC CRUD

--- app/views/edituser.php
+++ app/views/edituser.php
@@ -8,7 +8,7 @@ include_once'../app/views/nav.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" ></script>
-    <title>Document</title>
+    <title>MVC</title>
 </head>
 <body>
     
